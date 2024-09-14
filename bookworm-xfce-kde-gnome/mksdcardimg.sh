c84942e39e702c9c85395d608174523ea5663ee4
philipchen888 <philipchen888@users.noreply.github.com>
2024-09-13 20:13:50 -0700

    change from extlinux to uefi, add ubuntu-uefi

--- bookworm-xfce-kde-gnome/mksdcardimg.sh
+++ bookworm-xfce-kde-gnome/mksdcardimg.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 sudo parted -s /dev/sda mklabel gpt
-sudo parted -s /dev/sda -- unit s mkpart rootfs 61440 -34s
+sudo parted -s /dev/sda unit s mkpart boot fat32 2048 2869247
+sudo parted -s /dev/sda -- unit s mkpart rootfs ext4 2869248 -34s
 sudo parted -s /dev/sda set 1 boot on
-sudo dd if=../rootfs-bookworm/linaro-basicrootfs.img of=/dev/sda bs=4096 seek=7680 conv=notrunc,fsync
+sudo dd if=../rootfs-bookworm/linaro-esp.img of=/dev/sda bs=4096 seek=256 conv=notrunc,fsync
+sudo dd if=../rootfs-bookworm/linaro-basicrootfs.img of=/dev/sda bs=4096 seek=358656 conv=notrunc,fsync
--- mk-rootfs-ubuntu.sh
+++ mk-rootfs-ubuntu.sh
@@ -34,6 +34,7 @@ sudo tar -xpf binary-tar.tar.gz
 
 sudo cp -rf ../linux/linux-rockchip/tmp/lib/modules $TARGET_ROOTFS_DIR/lib
 sudo cp -rf ../linux/linux-rockchip/tmp/boot/* $TARGET_ROOTFS_DIR/boot
+sudo cp ../linux/patches/40_custom_uuid $TARGET_ROOTFS_DIR/boot
 
 # overlay folder
 sudo cp -rf ../overlay/* $TARGET_ROOTFS_DIR/
@@ -44,7 +45,10 @@ if [ "$ARCH" == "armhf" ]; then
 elif [ "$ARCH" == "arm64"  ]; then
 	sudo cp /usr/bin/qemu-aarch64-static $TARGET_ROOTFS_DIR/usr/bin/
 fi
+sudo mount -o bind /proc $TARGET_ROOTFS_DIR/proc
+sudo mount -o bind /sys $TARGET_ROOTFS_DIR/sys
 sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
+sudo mount -o bind /dev/pts $TARGET_ROOTFS_DIR/dev/pts
 
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
 
@@ -59,7 +63,32 @@ apt-get -y install libmali-g610-x11
 apt-get update
 apt-get upgrade -y
 apt-get -y dist-upgrade
-apt-get install -y build-essential git wget camera-engine-rkaiq-rk3588 wiringpi-opi libwiringpi2-opi libwiringpi-opi-dev
+apt-get install -y build-essential git wget camera-engine-rkaiq-rk3588 wiringpi-opi libwiringpi2-opi libwiringpi-opi-dev grub-efi-arm64
+
+# Install and configure GRUB
+mkdir -p /boot/efi
+grub-install --target=arm64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+update-grub
+
+cp /boot/40_custom_uuid /etc/grub.d/
+chmod +x /etc/grub.d/40_custom_uuid
+rm -rf /boot/40_custom_uuid
+
+# Migrate extlinux.conf to GRUB
+rm -rf /boot/extlinux
+cat << GRUB_EOF > /etc/default/grub
+GRUB_DEFAULT="Boot from UUID"
+GRUB_TIMEOUT=20
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX=""
+GRUB_EOF
+
+cat << FSTAB_EOF > /etc/fstab
+UUID=0c21ae25-58aa-4153-87f6-ea31b7a8e0f5 /  ext4    errors=remount-ro   0   1
+UUID=95E4-6EA5  /boot/efi  vfat    umask=0077      0       1
+FSTAB_EOF
+
+update-grub
 
 chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper
 chmod +x /etc/rc.local
@@ -73,4 +102,7 @@ rm -rf /var/lib/apt/lists/*
 
 EOF
 
+sudo umount $TARGET_ROOTFS_DIR/proc
+sudo umount $TARGET_ROOTFS_DIR/sys
+sudo umount $TARGET_ROOTFS_DIR/dev/pts
 sudo umount $TARGET_ROOTFS_DIR/dev
